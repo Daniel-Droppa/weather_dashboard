4d311dac3dc013bbb549c4041f07442189a9c36d
Daniel Droppa <danwdroppa@gmail.com>
2020-02-12 15:34:15 -0500

    set up jquery and weather api, data can be entered into search box and data is console logged

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,12 +1,19 @@
 $("button").on("click",function() {
-var location = $("#").val()
+var location = $("#location").val()
 console.log(location);
+var savedBox = $("<div>")
+$(savedBox).addClass("location")
+var storeIt =savedBox.text(location)
+$(savedBox).append(storeIt)
+$("#savedLocations").prepend(savedBox)
+
 
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&units-imperial&APPID=67440f53bee9c0d87507f4ae95fa3a98"
 $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
+console.log(response);
 
-  }
+  })
 })
