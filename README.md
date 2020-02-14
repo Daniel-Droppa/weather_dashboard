24ad1c6e1cbd41d6742f3625ef987b09d428f0d0
Daniel Droppa <danwdroppa@gmail.com>
2020-02-14 07:18:07 -0500

    forgot ro add readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # weather_dashboard
+This is a simple weather app where user can inpute the name of a city and will find current weather in that city. past searches are saved to local storage with a max of 11 past searches, and when past searches are clicked it will rerun the search for that area.
+
+[link to github page](https://github.com/Daniel-Droppa/weather_dashboard)
+
+![screenshot of work day scheduler](./assets/images/weather-dashboard-screenshot.png)
--- assets/script/script.js
+++ assets/script/script.js
@@ -33,7 +33,7 @@ $("#searchBtn").on("click", function (event) {
     savedSearch()
     localStorage.setItem("oldlocation", JSON.stringify(savedLocations))
     console.log(savedLocations);
-    
+
 
 
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&units=imperial&APPID=67440f53bee9c0d87507f4ae95fa3a98"
@@ -45,12 +45,12 @@ $("#searchBtn").on("click", function (event) {
       var d = new Date();
       var month = d.getMonth() + 1;
       var day = d.getDate();
-      var output = (month < 10 ? '0' : '') + month +'/' +(day < 10 ? '0' : '')+ day + '/' +d.getFullYear() + 10n;
+      var output = (month < 10 ? '0' : '') + month + '/' + (day < 10 ? '0' : '') + day + '/' + d.getFullYear() + 10n;
 
-      var cityNameEl = $("<h3>" + response.name + " " + "("+output +")"+ "<h3>")
-      var temperature =$("<p>"+ "temperature: "+ response.main.temp+ "&#176"+ "F"+"<p>" )
-      var humidity = $("<p>"+ "humidity: "+ response.main.humidity+ "%"+"<p>" )
-      var wind = $("<p>"+ "Wind Speed: "+ response.wind.speed+ "M/H"+"<p>" )
+      var cityNameEl = $("<h3>" + response.name + " " + "(" + output + ")" + "<h3>")
+      var temperature = $("<p>" + "temperature: " + response.main.temp + "&#176" + "F" + "<p>")
+      var humidity = $("<p>" + "humidity: " + response.main.humidity + "%" + "<p>")
+      var wind = $("<p>" + "Wind Speed: " + response.wind.speed + "M/H" + "<p>")
 
 
       $("#cityArea").append(cityNameEl)
@@ -64,8 +64,37 @@ $("#searchBtn").on("click", function (event) {
 })
 
 
-$("#clear").on("click", function(event){
+$("#clear").on("click", function (event) {
   event.stopPropagation();
   localStorage.clear()
   $("#savedLocations").html("")
+})
+
+
+$(".location").on("click", function (event) {
+  event.stopPropagation();
+  $("#cityArea").html("")
+  var place = $(this).text()
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + place + "&units=imperial&APPID=67440f53bee9c0d87507f4ae95fa3a98"
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+    var d = new Date();
+    var month = d.getMonth() + 1;
+    var day = d.getDate();
+    var output = (month < 10 ? '0' : '') + month + '/' + (day < 10 ? '0' : '') + day + '/' + d.getFullYear() + 10n;
+
+    var cityNameEl = $("<h3>" + response.name + " " + "(" + output + ")" + "<h3>")
+    var temperature = $("<p>" + "temperature: " + response.main.temp + "&#176" + "F" + "<p>")
+    var humidity = $("<p>" + "humidity: " + response.main.humidity + "%" + "<p>")
+    var wind = $("<p>" + "Wind Speed: " + response.wind.speed + "M/H" + "<p>")
+
+
+    $("#cityArea").append(cityNameEl)
+    $("#cityArea").append(temperature)
+    $("#cityArea").append(humidity)
+    $("#cityArea").append(wind)
+  })
 })
